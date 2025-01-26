f917ddccf202e938609fa05db1833cd5f6245416
Hayat Ahmad <sayed.hayat05@hotmail.com>
2025-01-26 01:56:42 -0500

    Added Title Label on Pause Screen

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,34 +1,26 @@
 extends Control
 
-func _ready():
-	$AnimationPlayer.play("RESET")
-
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
+var _is_paused:bool = false:
+	set = set_paused
 	
-func pause():
-	get_tree().paused = true
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Escape"):
+		_is_paused = !_is_paused
+
+func set_paused(value:bool) ->void:
+	_is_paused = value
+	get_tree().paused = _is_paused
+	visible = _is_paused
 	$AnimationPlayer.play("blur")
 
-func testEsc():
-	if Input.is_action_just_pressed("Escape") and !get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("Escape") and get_tree().paused:
-		resume()
-		
-
 func _on_resume_pressed() -> void:
-	resume()
+	_is_paused = false
 
 
 func _on_restart_pressed() -> void:
-	resume()
+	_is_paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	
-func _process(delt):
-	testEsc()
