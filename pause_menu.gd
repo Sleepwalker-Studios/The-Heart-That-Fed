2b1c088733cf4042dfec408a59c753ef666f69e7
Hayat Ahmad <sayed.hayat05@hotmail.com>
2025-01-26 14:38:08 -0500

    Removed blur from pause menu - Added pause button functionality - Removed ESC button support

--- pause_menu.gd
+++ pause_menu.gd
@@ -2,29 +2,34 @@ extends Control
 
 var _is_paused:bool = false:
 	set = set_paused
-	
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Escape"):
-		_is_paused = !_is_paused
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 
 func set_paused(value:bool) ->void:
 	_is_paused = value
 	get_tree().paused = _is_paused
-	visible = _is_paused
-	$AnimationPlayer.play("blur")
+	if _is_paused:
+		show()
+	else:
+		hide()
+
+func toggle_pause() -> void:
+	_is_paused = !_is_paused
 
 func _on_resume_pressed() -> void:
 	_is_paused = false
-
 
 func _on_restart_pressed() -> void:
 	_is_paused = false
 	get_tree().reload_current_scene()
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_pause_pressed():
-	set_paused(true)
+	if _is_paused:
+		_is_paused = false
+	else:
+		_is_paused = true
