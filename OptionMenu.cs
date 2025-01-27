c547e1a2b10806c5d4be6373cd72a33f2402d8aa
Hayat Ahmad <sayed.hayat05@hotmail.com>
2025-01-26 21:07:06 -0500

    Added Back Button to Options Menu

--- OptionMenu.cs
+++ OptionMenu.cs
@@ -5,12 +5,13 @@ public partial class OptionMenu : Control
 {
 	public override void _Ready()
 	{
-		Button BackButton = GetTree().Root.GetNode<Button>("Control/MarginContainer/VBoxContainer/Back");
+		Button BackButton = GetTree().Root.GetNode<Button>("Control/CanvasLayer/Back");
 		BackButton.Pressed += OnBackPressed;
 	}
 
 	void OnBackPressed()
 	{
+		GD.Print("Back button pressed!"); // Add this line
 		GetTree().ChangeSceneToFile("res://Main.tscn");
 	}
-};
+}
