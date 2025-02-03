008d5831816aa50da66e1ee4d5bf020e5242f318
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-02-03 01:18:39 -0500

    Changed win scene

--- Pan.cs
+++ Pan.cs
@@ -3,19 +3,24 @@ using System;
 
 public partial class Pan : Camera2D
 {
+	public bool wait;
 	public bool quit;
 	public bool start;
 	public Vector2 pos;
 	public float pan;
 	public Timer _delay;
 	public Timer _quit;
+	public Timer _wait;
 	
 	public override void _Ready()
 	{
 		_delay = GetTree().Root.GetNode<Timer>("Control/Timer");
 		_quit = GetTree().Root.GetNode<Timer>("Control/Timer2");
+		_wait = GetTree().Root.GetNode<Timer>("Control/Timer3");
+		_wait.Start();
 		_delay.Start();
 		_delay.Timeout += OnTimeout;
+		_wait.Timeout += OnWaitTimeout;
 		_quit.Timeout += OnQuitTimeout;
 		_quit.Start();
 		pos = Position;
@@ -29,7 +34,10 @@ public partial class Pan : Camera2D
 			pan = Mathf.Clamp(pan, 0f, 1f);
 			pos.Y = Mathf.Lerp(pos.Y, 190f, pan);
 			Position = pos;
-			Zoom = Zoom.Lerp(new Vector2(8,8), (float)(0.25 * delta));
+			if(wait)
+			{
+				Zoom = Zoom.Lerp(new Vector2(8,8), (float)(0.25 * delta));
+			}
 		}
 		if(pan >= 1f)
 		{
@@ -48,5 +56,10 @@ public partial class Pan : Camera2D
 	public void OnQuitTimeout()
 	{
 		quit = true;
+	}
+	
+	public void OnWaitTimeout()
+	{
+		wait = true;
 	}
 }
